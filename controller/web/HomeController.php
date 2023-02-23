c1d434e8fdac051e9bbe42c94f9f5efe131c4f33
Denetor <gfc-com@mail.ru>
2023-02-23 13:21:58 +0300

    add SK params to default home page

--- controller/web/HomeController.php
+++ controller/web/HomeController.php
@@ -3,14 +3,13 @@
 namespace controller\web;
 
 use sketch\controller\ControllerBase;
+use sketch\SK;
 
 class HomeController extends ControllerBase
 {
 
     public function actionIndex(){
-
-        return $this->render('home/index.php');
-
+       return $this->render('home/index.php', ['SK_props' => SK::getProps()]);
     }
 
 
